5c1bd240824e7d9b41f35ceb06f9acfdab26cdf0
Amanda <amandayu255@gmail.com>
2024-09-27 11:46:45 -0700

    added function to find users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -47,17 +47,30 @@ const users = {
 
 const findUserByName = (name) => {
     return users["users_list"].filter(
-      (user) => user["name"] === name
+        (user) => user["name"] === name
     );
-  };
-  
-  app.get("/users", (req, res) => {
+};
+
+app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
     } else {
-      res.send(users);
+        res.send(users);
     }
-  });
+});
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
