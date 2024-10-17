5b522ed6d620c172c7c1f11626e8ed2c50d3e0af
Amanda <amandayu255@gmail.com>
2024-10-16 17:57:21 -0700

    added id

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -4,8 +4,10 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>ID</th> 
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
@@ -15,14 +17,16 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+        <td>{row.id}</td>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>
-          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        <button onClick={() => props.removeCharacter(index)}>Delete</button>
         </td>
       </tr>
     );
   });
+
   return <tbody>{rows}</tbody>;
 }
 
